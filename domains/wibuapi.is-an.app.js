ad93dcead8012d2d685e24d8f894705c0e243113
Yoga Pranata <ahmadzaylani001@gmail.com>
2023-10-16 20:50:43 +0700

    Register wibuapi.is-an.app

---
+++ domains/wibuapi.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'Rest API website for weebs',
+  domain: 'is-an.app',
+  subdomain: 'wibuapi',
+  owner: {
+    email: 'ahmadzaylani001@gmail.com',
+  },
+  record: {
+    CNAME: 'wibuapi.onrender.com',
+  },
+  proxy: true,
+})
